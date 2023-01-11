a7ba2ebe7752e0595dea0a17b20a9abf65b4ed47
loveyrooney <loveyrooney@gmail.com>
2023-01-12 00:18:42 +0900

    feat: jebi backend finish

--- pickhelper/controller/Csadari.js
+++
@@ -1,3 +0,0 @@
-exports.sadari_home = (req,res) => {
-    res.render('sadari');
-}
--- pickhelper/routes/index.js
+++ pickhelper/routes/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/Cmain');
-const controller2 = require('../controller/Csadari');
+const controller2 = require('../controller/Cjebi');
 const controller3 = require('../controller/Croulette');
 const controller4 = require('../controller/Cdungle');
 const controller5 = require('../controller/Ctournament');
 const controller6 = require('../controller/Cguest');
 
 router.get("/", controller.main);
-router.get("/sadari", controller2.sadari_home);
+router.get("/jebi", controller2.jebi_home);
 router.get("/roulette", controller3.roulette_home);
 router.get("/dungle", controller4.dungle_home);
 router.get("/tournament", controller5.tournament_home);
 router.get("/guest", controller6.guest_home);
+
+router.get("/jebimake", controller2.jebi_make);
 
 module.exports = router;
